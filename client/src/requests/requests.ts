7bb1e555ffaf13262df8157a2e0b5c1f938728e5
sixilli <alecperro@gmail.com>
2023-02-16 21:00:32 -0600

    build stuff

--- client/src/requests/requests.ts
+++ client/src/requests/requests.ts
@@ -1,6 +1,9 @@
+import { Team } from "../models/rapidapi";
+
 const urlBase = import.meta.env.API_URL;
 
-export async function requestTeams(): Promise<any> {
-  let resp = fetch(urlBase + "/teams");
-  return resp;
+export async function requestTeams(): Promise<Team[]> {
+  const response = await fetch(urlBase + "/teams");
+  const teams = (await response.json()) as Team[];
+  return teams;
 }
